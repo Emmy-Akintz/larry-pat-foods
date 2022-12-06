9d09070e4255b11a0a3221810a0e40f2f10040ea
Emmy-Akintz <115672480+Emmy-Akintz@users.noreply.github.com>
2022-12-06 16:25:47 +0100

    Update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,14 +6,16 @@ function home() {
     <>
       <div className='h-auto md:h-[100vh] lg:h-[100vh] bg-gray-100 text-black block md:flex lg:flex'>
         <div className="block my-auto w-[100%] md:w-[50%] lg:w-[50%] text-left p-20 pb-0 md:pr-0 lg:pr-0">
-          <div className="bg-green-500 w-7 h-7 rounded-full animate-ping"></div>
-          <h1 className="larry-pat mt-8 font-bold text-5xl">Larry-Pat</h1>
-          <h1 className="foods font-bold text-5xl">Foods</h1>
-          <p className='under-foods mt-4'>The best quality food product there is</p>
-          <button className='mt-8 bg-green-500 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white animate-bounce'>ORDER NOW</button>
+          <div className="bg-green-500 w-8 h-8 rounded-full animate-ping"></div>
+          <h1 className="larry-pat mt-8 font-bold text-5xl md:text-7xl lg:text-7xl">Larry-Pat</h1>
+          <h1 className="foods font-bold text-5xl md:text-7xl lg:text-7xl">Foods</h1>
+          <p className='under-foods mt-4 text-xl md:text-2xl lg:text-2xl text-center md:text-left lg:text-left'>The best quality food product there is</p>
+          <div className="text-center md:text-left lg:text-left">
+            <button id='order-button' className='mt-16 bg-green-500 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-xl md:text-2xl lg:text-2xl animate-bounce'>ORDER NOW</button>
+          </div>
         </div>
-        <div className="first-image block md:my-auto lg:my-auto w-[100%] md:w-[50%] lg:w-[50%] p-20 md:pl-0 lg:pl-0 pt-10 md:pt-20 lg:pd-20">
-          <img src="images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png" alt="" className='animate-pulse first-image-itself' />
+        <div className="first-image block md:my-auto lg:my-auto w-[100%] md:w-[50%] lg:w-[50%] p-20 md:pl-0 lg:pl-0 pt-5 md:pt-20 lg:pt-20">
+          <img src="images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png" alt="" className='animate-pulse w-[150%] md:w-[100%] lg:w-[100%] first-image-itself' />
         </div>
       </div>
     </>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -7,24 +7,24 @@ function products() {
   return (
     <>
       <div className="h-auto md:h-[100vh] lg:h-[100vh] bg-gray-100 text-black py-20">
-        <h1 className="prod-1 font-bold text-5xl">What We Offer</h1>
+        <h1 className="prod-1 7font-bold text-5xl">What We Offer</h1>
         <p className="">Curious? Here are our most popular items.</p>
         <div className="block md:flex lg:flex justify-between w-5/6 mx-auto mt-8">
-          <div className="bg-white w-[200px] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
+          <div className="bg-white w-[150px] md:w-[25%] lg:w-[30%] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
             <img src="images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png" alt="" />
             <p className="font-bold">SALAD MIX</p>
             <IconContext.Provider value={{ color: 'black' }}>
               <FaArrowDown className='mx-auto mt-4 animate-bounce' />
             </IconContext.Provider>
           </div>
-          <div className="bg-white w-[200px] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
+          <div className="bg-white w-[150px] md:w-[25%] lg:w-[30%] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
             <img src="images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png" alt="" />
             <p className="font-bold">SALAD MIX</p>
             <IconContext.Provider value={{ color: 'black' }}>
               <FaArrowDown className='mx-auto mt-4 animate-bounce' />
             </IconContext.Provider>
           </div>
-          <div className="bg-white w-[200px] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
+          <div className="bg-white w-[150px] md:w-[25%] lg:w-[30%] rounded-xl border-2 border-white hover:border-gray-300 transition-all p-4 my-4 mx-auto">
             <img src="images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png" alt="" />
             <p className="font-bold">SALAD MIX</p>
             <IconContext.Provider value={{ color: 'black' }}>
