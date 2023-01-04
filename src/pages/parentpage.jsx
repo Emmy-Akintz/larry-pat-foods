90302dfb81417658d60833200c8ba1e4510a7e8e
Emmy-Akintz <emzyakints2005@gmail.com>
2023-01-04 20:05:10 +0100

    Update

--- src/pages/parentpage.jsx
+++ src/pages/parentpage.jsx
@@ -6,6 +6,7 @@ import Review from '../pages/review'
 import Contact from '../pages/contact'
 import { Link } from 'react-router-dom'
 import { FaArrowAltCircleUp } from 'react-icons/fa'
+import '../App.css'
 
 function parentpage() {
   return (
@@ -17,7 +18,7 @@ function parentpage() {
       <Review />
       <Contact />
       <Link to='/login' className="absolute top-4 right-4 bg-green-500 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-sm animate-bounce">LOGIN</Link>
-      <a href='#top' className='right-4 bottom-4 p-4 bg-green-600 hover:bg-green-500 rounded fixed justify-center bottom-4 w-12'>
+      <a href='#top' className='back-to-top right-4 bottom-4 p-4 bg-green-600 hover:bg-green-500 rounded fixed justify-center bottom-4 w-12'>
         <FaArrowAltCircleUp />
       </a>
     </div>
