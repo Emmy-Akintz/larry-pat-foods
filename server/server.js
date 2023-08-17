8e04a8f86b597fa565781c59300aa46b0274773c
Ayomide Akintan <emzyakints2005@gmail.com>
2023-08-17 21:19:03 +0100

    Update. database now accepts all data(tested through postman). next up is login

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+// const bodyParser = require('body-parser')
 const larrypatUsers = require('./routes/users')
 
 // express app
@@ -11,6 +12,7 @@ const app = express()
 // middleware
 app.use(cors())
 app.use(express.json())
+// app.use(bodyParser.json())
 
 app.use((res, req, next) => {
     console.log(req.path, req.method)
