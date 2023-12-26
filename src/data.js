185110f42573d0b4d9c55695d4007ca0fe8a0ff9
Ayomide Akintan <emzyakints2005@gmail.com>
2023-12-26 16:31:23 +0100

    update

--- src/data.js
+++ src/data.js
@@ -2,132 +2,154 @@ export const prod = [
     {
         id: 1,
         name: 'GROUNDNUT BALLS',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 2,
         name: 'HEALTHY CHIN CHIN',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 3,
         name: 'SUYA SPICE KULI KULI',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 4,
         name: 'CHIN CHIN SACHET',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 5,
         name: 'DETOX SOUP POWDER (GBANUNU)',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 6,
         name: 'DRIED CATFISH',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 7,
         name: 'HYGIENIC GARRI',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 8,
         name: 'TRADITIONALLY FERMENTED CASSAVA FLOUR (PUPURU)',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 9,
         name: 'PICKED HONEY BEANS',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 10,
         name: 'PEALED HONEY BEANS',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 11,
         name: 'PALM OIL (LOW FFA)',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 12,
         name: 'SOYA BEANS POWDER',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 13,
         name: 'SOYOAT MEAL',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 14,
         name: 'OATPLAN MEAL',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 15,
         name: 'PLANTAIN FLOUR',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 16,
         name: 'ORGANIC ROLLED OAT',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 17,
         name: 'BLENDED OAT',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 18,
         name: 'POTATO FLOUR',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 19,
         name: 'PLANTAIN FLOUR',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 20,
         name: 'BITTER GOURD POWDER (EJIRIN)',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 21,
         name: 'DRIED BITTER LEAF',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     },
 
     {
         id: 22,
         name: 'SCENT LEAF POWDER (EFINRIN)',
-        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png'
+        image: './images/anh-nguyen-kcA-c3f_3FE-unsplash-removebg-preview.png',
+        description: "This is just a template for larrypat foods products."
     }
 ];
