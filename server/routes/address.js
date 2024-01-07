fcb31af4de5b5abbc5c60adf1683f1f4b362d102
Ayomide Akintan <emzyakints2005@gmail.com>
2024-01-07 10:36:46 +0100

    added address route

---
+++ server/routes/address.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const requireAuth = require('../middleware/requireAuth')
+
+// controller functions
+
+const router = express.Router()
+
+router.use(requireAuth)
+
+// get user's address
+router.get('/get-address/:userId', () => {})
+
+// update user's address
+router.patch('/update-address/:userId', () => {})
+
+// delete user's address
+router.delete('/delete-address/:userId', () => {})
