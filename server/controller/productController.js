537acf5a656da95ffcf6aabca95ab11c922913cf
Ayomide Akintan <emzyakints2005@gmail.com>
2024-01-08 10:06:05 +0100

    adjusted the error message

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -61,13 +61,13 @@ const deleteProduct = async (req, res) => {
     const { id } = req.params
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: 'No such workout' })
+        return res.status(404).json({ error: 'No such product' })
     }
 
     const product = await Product.findOneAndDelete({ _id: id })
 
     if (!product) {
-        return res.status(400).json({ error: 'No such workout' })
+        return res.status(400).json({ error: 'No such product' })
     }
 
     res.status(200).json({ product })
