6c5f2b5d96f99f4f5bdc75cd83107d72e106d9e5
Ayomide Akintan <emzyakints2005@gmail.com>
2024-01-10 10:34:56 +0100

    restructuring userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const { addressSchema } = require('./addressModel')
 
 const userSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['manager', 'admin', 'client'], default: 'client' },
     registrationDate: { type: Date, default: Date.now },
     addresses: [addressSchema],
 }, { timestamps: true });
