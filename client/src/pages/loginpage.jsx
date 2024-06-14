8aba1dc490bd22c44b5400cf31a8b0093c3333af
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-06-14 10:51:09 +0100

    Updated the loginpage for medium and mobile devices

--- client/src/pages/loginpage.jsx
+++ client/src/pages/loginpage.jsx
@@ -1,77 +1,122 @@
-import { IconContext } from 'react-icons'
-import { FaHome, FaTimes } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
-import '../App.css'
-import { useLogin } from '../hooks/useLogin'
-import { useEffect, useState } from 'react'
-import { useAuthContext } from '../hooks/useAuthContext'
+import { IconContext } from "react-icons";
+import { FaHome, FaTimes } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import Input from "./components/Input";
 
 function Loginpage() {
-    const { user } = useAuthContext ()
+  const { user } = useAuthContext();
 
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const { login, error, isLoading } = useLogin()
-    const navigate = useNavigate()
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const { login, error, isLoading } = useLogin();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        await login(email, password)
+    await login(email, password);
+  };
+
+  useEffect(() => {
+    if (user) {
+      if (user.user.role === "manager") {
+        navigate("/manager-dashbord");
+      } else if (user.user.role === "admin") {
+        navigate("/admin-dashbord");
+      } else if (user.user.role === "client") {
+        navigate("/");
+      }
     }
+  }, [user]);
 
-    useEffect(() => {
-        if (user) {
-            if (user.user.role === "manager") {
-                navigate('/manager-dashbord')
-            } else if (user.user.role === "admin") {
-                navigate('/admin-dashbord')
-            } else if (user.user.role === "client") {
-                navigate('/')
-            }
-        }
-    }, [user])
-
-    return (
-        <div className='logsign p-4 bg-gray-200 h-[100vh]'>
-            <Link to='/'>
-                <div className="p-4 bg-green-500 hover:bg-green-300 w-12 rounded">
-                    <IconContext.Provider value={{ color: 'white' }}>
-                        <FaHome />
-                    </IconContext.Provider>
-                </div>
-            </Link>
-            <br />
-            <form action="/" className='rounded-xl w-[300px] md:w-[400px] lg:w-[500px] m-auto p-4 bg-green-100' onSubmit={handleSubmit}>
-                <h1 className='font-bold text-xl'>LOGIN TO LARRY-PAT FOODS</h1>
-                <br />
-                <label htmlFor="email">Email: </label>
-                <br className="block md:hidden lg:hidden" />
-                <input type="email" id='email' className='rounded border-gray-700 px-2 py-1 w-[200px] md:w-[200px] lg:w-[200px]' placeholder='' name='email' onChange={(e) => setEmail(e.target.value)} required />
-                <br />
-                <br />
-                <label htmlFor="password">Password: </label>
-                <br className="block md:hidden lg:hidden" />
-                <input type="password" id='password' className='rounded border-gray-700 px-2 py-1 w-[200px] md:w-[200px] lg:w-[200px]' placeholder='' name='password' onChange={(e) => setPassword(e.target.value)} required />
-                <br />
-                <br />
-                <button type="reset" title='Clear form'><FaTimes /></button>
-                <br />
-                <button type="submit" disabled={isLoading} className={isLoading ? 'bg-green-300 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-sm' : 'bg-green-500 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-sm'}>LOGIN</button>
-                <div className="error text-red-500">
-                    {error && <div className='error'>{error}</div>}
-                </div>
-                <br />
-                <br />
-                <p>Forgotten <Link to='/forgotPass' className='text-green-700 font-bold hover:underline'>PASSWORD</Link></p>
-                <br />
-                <hr />
-                <br />
-                <p>Don't have an account?</p>
-            </form>
-            <p>SignUp <Link to='/signup' className='text-green-700 font-bold hover:underline'>HERE</Link></p>
+  return (
+    <div className="min-h-screen md:logsign py-2 px-2 bg-gray-200 lg:flex lg:flex-col lg:justify-around">
+      <Link to="/">
+        <div className="p-4 bg-green-500 hover:bg-green-300 w-12 rounded-full md:rounded translate-y-20 md:translate-y-0 translate-x-4 md:translate-x-0">
+          <IconContext.Provider value={{ color: "white" }}>
+            <FaHome />
+          </IconContext.Provider>
         </div>
-    );
+      </Link>
+      <br />
+      <form
+        action="/"
+        className="rounded-xl w-[300px] md:w-[400px] lg:w-[700px] lg:h-[550px] md:m-auto p-4 bg-[rgb(110,189,138)] pb-1"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="font-bold text-xl lg:text-5xl uppercase border-b-2 pb-4">
+          Login
+        </h1>
+        <br />
+        <Input
+          type="email"
+          id="email"
+          label="Email: "
+          classes=" font-bold text-2xl"
+          stater={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          id="password"
+          label="Password: "
+          classes="font-bold text-2xl"
+          stater={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <div className="flex justify-center items-center pt-4 md:pt-6">
+          <p className="mr-3 md:mr-4 lg:mr-8 xl:mr-10 font-semibold text-red-600 lg:text-3xl">
+            Clear form?
+          </p>
+          <button
+            type="reset"
+            title="Clear form"
+            className="bg-red-500 p-2 md:p-3 rounded text-white lg:p-5 lg:rounded-md"
+          >
+            <FaTimes />
+          </button>
+        </div>
+        <br />
+        <button
+          type="submit"
+          disabled={isLoading}
+          className={
+            isLoading
+              ? "bg-green-300 hover:bg-green-400 transition-all py-2 px-6 font-semibold rounded-3xl text-white text-base mb-6 lg:py-5 lg:px-14  lg:rounded-3xl lg:text-2xl"
+              : "bg-green-500 hover:bg-green-400 transition-all py-2 px-6 font-semibold rounded-3xl text-white text-base mb-6 lg:py-5 lg:px-14  lg:rounded-3xl lg:text-2xl"
+          }
+        >
+          LOGIN
+        </button>
+        <div className="error text-red-500">
+          {error && <div className="error">{error}</div>}
+        </div>
+        <p className="  border-t-gray-200 border-t-2 pt-6 mb-6 lg:text-2xl">
+          Forgot Your{" "}
+          <Link
+            to="/forgotPass"
+            className="text-green-700 font-bold md:hover:underline underline underline-offset-4"
+          >
+            PASSWORD?
+          </Link>
+        </p>
+      </form>
+      <div className="bg-[rgb(110,189,138)] mt-4 md:mt-10 md:mb-4 rounded-md py-6 px-2 font-semibold w-[300px] md:w-[400px] lg:w-[700px] md:m-auto md:rounded-xl">
+        <p>Don't have an account?</p>
+        <p>
+          Sign Up{" "}
+          <Link
+            to="/signup"
+            className="text-green-700 font-bold md:hover:underline underline underline-offset-4"
+          >
+            HERE
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Loginpage    
+export default Loginpage;
--- client/src/pages/signuppage.jsx
+++ client/src/pages/signuppage.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react'
 import { IconContext } from 'react-icons'
 import { FaHome, FaTimes } from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
-import '../App.css'
 import axios from 'axios'
 import { useSignup } from '../hooks/useSignup'
-import InputMaker from './components/Input'
+import Input from './components/Input'
 
 export default function Signuppage() {
     const [email, setEmail] = useState("")
@@ -25,7 +24,7 @@ export default function Signuppage() {
     }
 
     return (
-        <div className='md:logsign px-4 py-12 pb-8  md:p-4  bg-gray-300 h-[120vh] md:h-[100vh] text-black'>
+        <div className='md:logsign px-2 pr-2 md:px-4 md:py-12 pb-8  md:p-4 bg-gray-300 h-[120vh] md:h-[120vh] lg:h-[100vh] text-black'>
             <Link to='/'>
                 <div className="p-4 bg-green-500 hover:bg-green-300 w-12 rounded-full md:rounded translate-y-14 md:translate-y-0 translate-x-4 md:translate-x-0">
                     <IconContext.Provider value={{ color: 'white' }}>
@@ -36,12 +35,12 @@ export default function Signuppage() {
             <form action="/" className='rounded-xl w-[300px] md:w-[400px] lg:w-[500px] md:m-auto p-4 bg-green-100' onSubmit={handleSubmit}>
                 <h1 className='font-bold text-xl uppercase border-b-2 pb-4'>signup page</h1>
                 <br />
-                <InputMaker label="First Name: " type="text" id="first_name" classes="" stater={() => setFirstName(e.target.value)} />
-                <InputMaker label="Last value: " type="text" id="last_name" classes="" stater={() => setLastName(e.target.value)} />
-                <InputMaker type="email" id="email" classes="" label="Email: " stater={() => setEmail(e.target.value)} />
-                <InputMaker type="password" id="password" label="Password: " stater={()=> setPassword(e.target.value)} />
+                <Input label="First Name: " type="text" id="first_name" classes="" stater={(e) => setFirstName(e.target.value)} />
+                <Input label="Last value: " type="text" id="last_name" classes="" stater={(e) => setLastName(e.target.value)} />
+                <Input type="email" id="email" classes="" label="Email: " stater={(e) => setEmail(e.target.value)} />
+                <Input type="password" id="password" label="Password: " stater={(e)=> setPassword(e.target.value)} />
                 <div className='flex justify-center items-center pt-4 md:pt-6'>
-                    <p className='mr-3 md:mr-4 lg:mr-8 xl:mr-10 font-semibold'>Clear form?</p>
+                    <p className='mr-3 md:mr-4 lg:mr-8 xl:mr-10 font-semibold text-red-600'>Clear form?</p>
                     <button type="reset" title='Clear form' className="bg-red-500 p-2 md:p-3 rounded text-white"><FaTimes /></button>
                 </div>
                 <br />
@@ -51,8 +50,8 @@ export default function Signuppage() {
                 <button type="submit" disabled={isLoading} className={isLoading ? 'bg-green-300 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-sm' : 'bg-green-500 hover:bg-green-400 transition-all py-2 px-4 rounded-3xl text-white text-sm'}>SIGN UP</button>
             </form>
             <div className='bg-green-100 mt-2 md:mt-[3vh] px-2 py-3 md:px-4 md:py-6 rounded-2xl w-[300px] md:w-[400px] lg:w-[500px] m-auto'>
-                <p>Already have an account?</p>
-                <p>Login <Link to='/login' className='text-green-500 font-bold hover:underline'>HERE</Link></p>
+                <p>Already have an Account?</p>
+                <p className='font-semibold'>Login <Link to='/login' className='text-green-500 font-bold underline md:no-underline hover:underline'>HERE</Link></p>
             </div>
         </div>
     )
